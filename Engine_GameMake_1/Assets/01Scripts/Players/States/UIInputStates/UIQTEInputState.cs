c2993f2ce65b01355006cff7b9d21a9cac2bf1bc
chuh007 <chhuh007@gmail.com>
2025-06-08 00:43:27 +0900

    feat : QTE, Asset
    폰트 넣으려고 일단 커밋함

--- Engine_GameMake_1/Assets/01Scripts/Players/States/UIInputStates/UIQTEInputState.cs
+++ Engine_GameMake_1/Assets/01Scripts/Players/States/UIInputStates/UIQTEInputState.cs
@@ -1,15 +1,18 @@
 ﻿using _01Scripts.Combat;
 using _01Scripts.Core;
 using _01Scripts.Entities;
+using QTESystem;
 using UnityEngine;
 
 namespace _01Scripts.Players.States.UIInputStates
 {
     public class UIQTEInputState : UIInputState
     {
+        private SlidingQTE _qteCompo;
         
         public UIQTEInputState(Entity entity, int animationHash) : base(entity, animationHash)
         {
+            _qteCompo = _player.GetCompo<SlidingQTE>();
         }
 
         public override void Enter()
@@ -17,12 +20,26 @@ namespace _01Scripts.Players.States.UIInputStates
             base.Enter();
             PlayerUIInoutComponent.InputUIChanged(ControlUIType.UIQTEInput);
             _player.PlayerBattleInput.OnQTEKeyPressed += HandleQTEPressed;
+            _qteCompo.onSuccess.AddListener(HandleSuccess);
+            _qteCompo.onFailure.AddListener(HandleFailure);
+            _qteCompo.BeginQTE();
+        }
+
+        private void HandleSuccess()
+        {
+            _player.ChangeState("ATTACKMOTION");
+        }
+        
+        private void HandleFailure()
+        {
+            _player.ChangeState("ATTACKMOTION");
         }
 
         private void HandleQTEPressed()
         {
             // QTE 진행, QTE 끝나면 공격 컴포넌트에 공격 명령 -- 새 상태 만들었. 거따 전이.
-            _player.ChangeState("ATTACKMOTION");
+            _qteCompo.HandleQTEPressed();
+            // _player.ChangeState("ATTACKMOTION");
         }
 
         public override void Exit()
